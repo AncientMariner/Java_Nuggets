ae6dda4a624921286ade9093163ac69dee1228d6
AncientMariner <xander.lysak@gmail.com>
2014-07-09 19:13:03 +0300

    trick with compiler and VM

---
+++ src/main/java/org/xander/TrickWithCompilerAndVirtualMachine.java
@@ -0,0 +1,30 @@
+package org.xander;
+
+public class TrickWithCompilerAndVirtualMachine {
+
+//  public static String getExample(List<String> list) {
+//    return list.get(0); // return first
+//  }
+//
+//  public static Integer getExample(List<Integer> list) {
+//    return list.get(list.size() - 1);  // return last
+//  }
+//
+//  public static void main(String[] args) {
+//    System.out.println(getExample(Arrays.asList("1", "2", "3")));
+//    System.out.println(getExample(Arrays.asList(1, 2, 3)));
+//  }
+
+//    public static String getExample(List list) {
+//        return (String) list.get(0); // return first
+//    }
+//
+//    public static Integer getExample(List list) {
+//        return (Integer) list.get(list.size() - 1);  // return last
+//    }
+//
+//    public static void main(String[] args) {
+//        System.out.println(getExample(Arrays.asList("1", "2", "3")));
+//        System.out.println(getExample(Arrays.asList(1, 2, 3)));
+//    }
+}
